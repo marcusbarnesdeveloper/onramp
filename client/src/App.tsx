a66d30bcc661100f4c3ab6232ccb1b7fc421d08b
marcusbarnesdeveloper <marcusbarnesdev@gmail.com>
2021-01-08 09:25:04 -0800

    init project

---
+++ client/src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Header from './Components/Header/header';
+import Footer from './Components/Footer/footer';
+import Main from './Components/Main/main';
+import Grid from '@material-ui/core/Grid';
+import  useStyles  from './AppCss';
+
+const App = (message: string) => {
+  const classes = useStyles();
+  return(
+      <Grid
+        container
+        direction="column"
+        justify="space-between"
+        alignItems="center"
+        className={classes.root}
+      >
+        <Header/>
+        <Main/>
+        <Footer/>
+      </Grid>
+  )
+}
+
+export default App;
