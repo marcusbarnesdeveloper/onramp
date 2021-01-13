d33089e3e818ed16ff9ac4980c08e48738cde22e
marcusbarnesdeveloper <marcusbarnesdev@gmail.com>
2021-01-12 17:03:50 -0800

    added backend routers to handle request for lat long and weather

---
+++ server/Controller.js
@@ -0,0 +1,10 @@
+
+const geocode = (city, state) => {
+  const url =  `https://api.opencagedata.com/geocode/v1/json?q=${city,state}&key=033195659d18444ba44c76525d8ad96f`;
+  return ;
+}
+
+
+module.exports = {
+  geocode,
+}
--- server/app.js
+++ server/app.js
@@ -2,6 +2,9 @@ var createError = require('http-errors');
 var express = require('express');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const Weather = require('./Controller.js');
+const axios = require('axios');
+
 
 var app = express();
 const port = 8080;
@@ -11,12 +14,41 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.set('view engine', 'html');
 
-app.get('/api', (req, res) => {
-  res.send(`${new Date()}`);
+app.get('/api/location', async (req, res) => {
+  try {
+    let city = req.query.city;
+    let state = req.query.state;
+    const url =  `https://api.opencagedata.com/geocode/v1/json?q=${city}&key=033195659d18444ba44c76525d8ad96f`;
+    let {data} = await axios.get(url);
+    if(data.status.code === 200) {
+      if(data.results.length > 0) {
+        const place = data.results[0];
+        const lat = place.lat;
+        const lng = place.lng;
+        res.send(place);
+      } else {
+        res.status(200).send({msg: 'location not found'});
+      }
+    } else {
+      res.status(500).send({msg:'internal error'})
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({msg:'internal error'})
+  }
 });
 
-app.get('/api/users', (req, res) => {
-  res.send(['Aang', 'Katara', 'Momo', 'Sokka', 'Appa']);
+app.get('/api/weather', async (req, res) => {
+  try {
+    let {lat, lon} = req.query;
+    const key = '0dc80bd45e6d151609478625bce41471';
+    const url =  `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${key}`;
+    let {data} = await axios.get(url);
+    res.send(data);
+  } catch(err) {
+    console.log(err);
+    res.status(500).send({msg:'invalid'});
+  }
 });
 
 // catch 404 and forward to error handler
